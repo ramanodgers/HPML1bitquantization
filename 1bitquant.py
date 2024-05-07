baaee860178e6c1d004d1c28733373fc3aa0656b
ramanodgers <raman.odgers@hotmail.com>
2024-05-06 23:30:47 -0400

    working classes

--- 1bitquant.py
+++ 1bitquant.py
@@ -7,10 +7,32 @@ import evaluate
 from transformers import TrainingArguments, Trainer
 import torch.nn as nn
 from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 from PTQutils import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+MAX = 127
+MIN = -128
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5, bias=False)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5, bias=False)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120, bias=False)
+        self.fc2 = nn.Linear(120, 84, bias=False)
+        self.fc3 = nn.Linear(84, 10, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 #general preliminaries 
 train_data = load_dataset("GATE-engine/mini_imagenet", split='train', trust_remote_code=True).select(range(2000))
@@ -39,6 +61,7 @@ def main():
     feature_extractor = AutoImageProcessor.from_pretrained(model_path)
     transform = get_transform(feature_extractor)
     model = AutoModelForImageClassification.from_pretrained(model_path).train().to(device)
+    # model =  Net().to(device)  
     output_dir = "./results/" + model_name
 
     train_dataset = train_data.with_transform(transform)
@@ -71,6 +94,7 @@ def main():
     print("Set learning rate to:", training_args.learning_rate)
 
     #initial training and eval 
+    print("\n\nINITIAL TRAINING AND EVAL\n\n")
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -78,15 +102,48 @@ def main():
         eval_dataset=test_dataset,
         compute_metrics=compute_metrics,
     )
-    trainer.train() 
-
-    #profiling
-    register_activation_profiling_hooks(model)
-    metrics = trainer.evaluate()
+    trainer.train()  
+    metrics = trainer.evaluate(val_dataset.select(range(400)))
     trainer.log_metrics("eval", metrics)
 
+    visualize_weights(model, './results/before.png')
+
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+    
+    def test(model: nn.Module, dataloader: DataLoader, max_samples=None) -> float:
+        correct = 0
+        total = 0
+        n_inferences = 0
+
+        with torch.no_grad():
+            for data in dataloader:
+                images, labels = data['pixel_values'], data['labels']
+                images = images.to(device)
+                labels = labels.to(device)
+
+                outputs = model(images)
+                _, predicted = torch.max(outputs.logits, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                if max_samples:
+                    n_inferences += images.shape[0]
+                    if n_inferences > max_samples:
+                        break
+
+        return 100 * correct / total
+    score = test(model, testloader)
+    print('Accuracy of the network on the test images: {}%'.format(score))
+
     #quantizing weights and testing
-    quantize_layer_weights(model)
+    print('\n\nWEIGHTS HAVE BEEN QUANTIZED, TESTING\n\n')
+    quantize_layer_weights(MAX, MIN, model, device)
+    visualize_weights(model, './results/after.png')
+    for name, layer in list(model.named_modules()):
+        if hasattr(layer, 'weight'):
+            print(name)
+            print(layer.weight.data)
     model2 = model
     trainer = Trainer(
         model=model2,
@@ -95,34 +152,38 @@ def main():
         eval_dataset=test_dataset,
         compute_metrics=compute_metrics,
     )
-    metrics = trainer.evaluate()
+    metrics = trainer.evaluate(val_dataset.select(range(400)))
+    trainer.log_metrics("eval", metrics)
+    
 
-    #quantizing activations and biases
-    model3 = NetQuantized(model2)
-    trainer = Trainer(
-        model=model3,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        compute_metrics=compute_metrics,
-    )
-    metrics = trainer.evaluate()
+    score = test(model2, testloader)
+    print('Accuracy of the network on the test images: {}%'.format(score))
+
+    # #profiling
+    # print("\n\nREGISTERED HOOKS, PASSING THROUGH FOR CALIBRATION\n\n")
+    # register_activation_profiling_hooks(model)
+    # metrics = trainer.evaluate(val_dataset.select(range(400)))
+    # model.profile_activations = False
+    # clear_activations(model)
+    # trainer.log_metrics("eval", metrics)
 
 
-    # score = test(net_with_bias_with_quantized_weights, testloader)
+    
+    # #quantizing activations and biases
+    # print('\n\nQUANTIZING ACTIVATIONS, TESTING\n\n')
+    # model3 = modelQuantized(MAX,MIN,model2)
+    # metric = evaluate.load("accuracy")
 
-    # #how to PTQquantise"
-    # ''' (TEST) register profiling hoooks, run a testing sweep, pass to quantize layer weights,(TEST) and then netquantized (TEST)'''
+    # trainer = Trainer(
+    #     model=model3,
+    #     args=training_args,
+    #     train_dataset=train_dataset,
+    #     eval_dataset=test_dataset,
+    #     compute_metrics=compute_metrics,
+    # )
+    # metrics = trainer.evaluate(val_dataset.select(range(400)))
+    # trainer.log_metrics("eval", metrics)
 
-    # register_activation_profiling_hooks(net_with_bias)
-    # test(net_with_bias, trainloader, max_samples=400)
-    # net_with_bias.profile_activations = False
-    # net_with_bias_with_quantized_weights = copy_model(net_with_bias)
-    # quantize_layer_weights(net_with_bias_with_quantized_weights)
-    # score = test(net_with_bias_with_quantized_weights, testloader)
-
-    # net_quantized_with_bias = NetQuantizedWithBias(net_with_bias_with_quantized_weights)
-    # score = test(net_quantized_with_bias, testloader)
 
 if __name__ == '__main__':
     main()
--- PTQutils.py
+++ PTQutils.py
@@ -2,101 +2,134 @@ import torch
 import torch.nn as nn
 from typing import Tuple, List 
 import numpy as np 
+import matplotlib.pyplot as plt
 
-def quantized_weights(weights: torch.Tensor) -> Tuple[torch.Tensor, float]:
-    '''
-    Quantize the weights so that all values are integers between -128 and 127.
-    You may want to use the total range, 3-sigma range, or some other range when
-    deciding just what factors to scale the float32 values by.
-
-    Parameters:
-    weights (Tensor): The unquantized weights
-
-    Returns:
-    (Tensor, float): A tuple with the following elements:
-                        * The weights in quantized form, where every value is an integer between -128 and 127.
-                          The "dtype" will still be "float", but the values themselves should all be integers.
-                        * The scaling factor that your weights were multiplied by.
-                          This value does not need to be an 8-bit integer.
-    '''
-    inv_scale = (1+1) / (torch.max(weights)-torch.min(weights))
-    zero_point = 0
+def quantized_weights(max, min, weights: torch.Tensor) -> Tuple[torch.Tensor, float]:
+    inv_scale = (max-min) / (torch.max(weights)-torch.min(weights))
+    zero_point = (min-torch.min(weights)*inv_scale).round()
+    # zero_point = 0
 
     result = (weights * inv_scale).round() + zero_point
-    return torch.clamp(result, min=-1, max=1), inv_scale
+    return torch.clamp(result, min=min, max=max), inv_scale
 
-#recursively searches for linear and conv layers
-def quantize_layer_weights(model: nn.Module, config):
-    for name, module in model.named_children():
-        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-            q_layer_data, scale = quantized_weights(module.weight.data)
-            q_layer_data = q_layer_data.to(config.device)
+def visualize_weights(model, save_path):
+    weight_list = []
+    for name,layer in list(model.named_modules()):
+        if hasattr(layer, 'weight'):
+            weight_list.extend(layer.weight.data.cpu().view(-1).numpy())
+
+    plt.hist(weight_list,  bins = 'auto', align='left', color='blue', edgecolor='black')
+
+    # Adding titles and labels
+    plt.title('Weights distribution')
+    plt.xlabel('Values')
+    plt.ylabel('Frequency')
+
+    # Show the plot
+    print('saved')
+    plt.savefig(save_path)
+
+def quantize_layer_weights(max, min, model: nn.Module, device):
+    for name, module in model.named_modules():
+        if hasattr(module, 'weight'):
+            q_layer_data, scale = quantized_weights(max, min, module.weight.data)
+            q_layer_data = q_layer_data.to(device)
             module.weight.data = q_layer_data
             module.weight.scale = scale
-
-            if (q_layer_data < -128).any() or (q_layer_data > 127).any():
+            if (q_layer_data < min).any() or (q_layer_data > max).any():
                 raise Exception("Quantized weights of {} layer include values out of bounds for an 8-bit signed integer".format(name))
             if (q_layer_data != q_layer_data.round()).any():
                 raise Exception("Quantized weights of {} layer include non-integer values".format(name))
-        else:
-            quantize_layer_weights(module)
+        # else:
+        #     quantize_layer_weights(max, min, module, device)
 
 #generalized hooking
-
 def register_activation_profiling_hooks(model: nn.Module):
-    model.input_activations = np.empty(0)
-    model.layer_activations = {}
-
     model.profile_activations = True
+    found_first = False
+    model.input_activation_bounds = [0,0]
 
-    def activations_hook(name):
-        def hook(layer, x, y):
+    def input_hook(module, input):
+        if model.profile_activations:
+            max = torch.max(input[0])
+            min = torch.min(input[0])
+            if min < model.input_activation_bounds[0]:
+                model.input_activation_bounds[0] = min
+            if max > model.input_activation_bounds[1]:
+                model.input_activation_bounds[1] = max
+
+    def activation_hook(layer):
+        if not hasattr(layer, 'activations'):
+            layer.activation_bounds = [0,0]
+
+        def hook(module, input, output):
+            # Directly append the flattened output to the list
             if model.profile_activations:
-                model.layer_activations[name] = np.append(model.layer_activations.get(name, np.empty(0)), x[0].cpu().view(-1))
+                max = torch.max(output)
+                min = torch.min(output)
+                if min < module.activation_bounds[0]:
+                    module.activation_bounds[0] = min
+                if max > module.activation_bounds[1]:
+                    module.activation_bounds[1] = max
+                # module.activations.append(activation)
+
         return hook
 
     for name, layer in model.named_modules():
         if isinstance(layer, (nn.Conv2d, nn.Linear)):
-            layer.register_forward_hook(activations_hook(name))
+            if not found_first: #pick a deit layer here and see what the input is in the input hook 
+                found_first = True
+                layer.register_forward_pre_hook(input_hook)
+            layer.register_forward_hook(activation_hook(layer))
 
+def clear_activations(model: nn.Module):
+    model.profile_activations = False
 
-class NetQuantized(nn.Module):
-    def __init__(self, net: nn.Module):
+class modelQuantized(nn.Module):
+    def __init__(self, max, min, net: nn.Module):
         super(NetQuantized, self).__init__()
         self.net = net
         self.quantized_layers = []
+        self.max = max
+        self.min = min
 
         # Identify and process each quantizable layer
-        for name, module in self.net.named_modules():
+        #removes the wrapper model 
+        for name, module in list(self.net.named_modules()):
+            self.register_pre_hooks(module)
             if isinstance(module, (nn.Conv2d, nn.Linear)):
                 self.quantized_layers.append(module)
-                self.register_pre_hooks(module)
+                # self.register_pre_hooks(module)
 
-        self.input_activations = net.input_activations
-        self.input_scale = self.quantize_initial_input(self.input_activations)
+        self.input_activation_bounds = net.input_activation_bounds
+        self.input_scale = self.quantize_initial_input(self.input_activation_bounds)
         self.setup_output_scales()
 
     def register_pre_hooks(self, module):
         # Define and register a forward pre-hook
-        def pre_hook(layer, x):
-            x = x[0]
-            if (x < -128).any() or (x > 127).any():
-                raise Exception("Input to {} layer is out of bounds for an 8-bit signed integer".format(layer.__class__.__name__))
+        def pre_hook(layer, input):
+            output_scale = getattr(layer, 'output_scale', 1)
+            x = input[0]
+            x = torch.clamp(torch.round(x * output_scale), min=self.min, max=self.max)
+            if (x < self.min).any() or (x > self.max).any():
+                raise Exception(f"Input to {layer.__class__.__name__} layer is out of bounds for an 8-bit signed integer")
             if (x != x.round()).any():
-                raise Exception("Input to {} layer has non-integer values".format(layer.__class__.__name__))
+                raise Exception(f"Input to {layer.__class__.__name__} layer has non-integer values")
+            return (x,) + input[1:] 
+
         module.register_forward_pre_hook(pre_hook)
 
     def setup_output_scales(self):
         # Calculate and set the output scales and quantize biases for quantizable layers
         preceding_layer_scales = []
         for layer in self.quantized_layers:
-            layer.output_scale = self.quantize_activations(layer.activations, layer.weight.scale, self.input_scale, preceding_layer_scales)
-            preceding_layer_scales.append((layer.weight.scale, layer.output_scale))
+            layer.output_scale = self.quantize_activations(getattr(layer, 'activation_bounds', [0,1]), getattr(layer, 'weight.scale', 1), self.input_scale, preceding_layer_scales)
+            preceding_layer_scales.append((getattr(layer, 'weight.scale', 1), layer.output_scale))
 
             if hasattr(layer, 'bias') and layer.bias is not None:
                 layer.bias.data = self.quantized_bias(
                     layer.bias.data,
-                    layer.weight.scale,
+                    getattr(layer, 'weight.scale', 1),
                     self.input_scale,
                     preceding_layer_scales
                 )
@@ -105,16 +138,14 @@ class NetQuantized(nn.Module):
                 if (layer.bias.data != layer.bias.data.round()).any():
                     raise Exception("Bias for layer {} has non-integer values".format(layer.__class__.__name__))
 
-    @staticmethod
-    def quantize_initial_input(pixels: np.ndarray) -> float:
-        return (127 + 128) / (np.max(pixels) - np.min(pixels))
+    def quantize_initial_input(self,bounds: np.ndarray) -> float:
+        return (self.max - self.min) / (bounds[1] - bounds[0])
 
-    @staticmethod
-    def quantize_activations(activations: np.ndarray, n_w: float, n_initial_input: float, ns: List[Tuple[float, float]]) -> float:
+    def quantize_activations(self,activation_bounds, n_w: float, n_initial_input: float, ns: List[Tuple[float, float]]) -> float:
         cumulative_scale = n_initial_input * n_w
         for weight_scale, output_scale in ns:
             cumulative_scale *= output_scale * weight_scale
-        return (127 + 128) / (np.max(activations) - np.min(activations)) / cumulative_scale
+        return (self.max - self.min) / (activation_bounds[1] - activation_bounds[0]) / cumulative_scale
 
     @staticmethod
     def quantized_bias(bias: torch.Tensor, n_w: float, n_initial_input: float, ns: List[Tuple[float, float]]) -> torch.Tensor:
@@ -123,15 +154,9 @@ class NetQuantized(nn.Module):
             cumulative_scale *= output_scale * weight_scale
         return torch.clamp((bias * cumulative_scale).round(), min=-2147483648, max=2147483647)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, pixel_values, labels = None) -> torch.Tensor:
+        x = pixel_values
         # Apply initial scaling
-        x = torch.clamp(torch.round(x * self.input_scale), min=-128, max=127)
-
-        # Process each module respecting the original network structure
-        modules = list(self.net.named_children())
-        for name, module in modules:
-            x = module(x)
-            if module in self.quantized_layers and hasattr(module, 'output_scale'):
-                x = torch.clamp(torch.round(x * module.output_scale), min=-128, max=127)
-
+        x = torch.clamp(torch.round(x * self.input_scale), min=self.min, max=self.max)
+        x = self.net(x, labels=labels)
         return x
